5d2793b8fe033deee54e5b8f203a26ffded660d0
saolini1933076 <133365462+saolini1933076@users.noreply.github.com>
2023-09-25 15:15:37 +0200

    Update linked_list.h
    aggiunto una intestazione di funzione

--- linked_list.h
+++ linked_list.h
@@ -18,3 +18,4 @@ ListItem* List_detach(ListHead* head, ListItem* item);
 ListItem* List_pushBack(ListHead* head, ListItem* item);
 ListItem* List_pushFront(ListHead* head, ListItem* item);
 ListItem* List_popFront(ListHead* head);
+int List_size(const ListHead* head);
