a776875e25f9e680ca8ec65c93478618c5c3c956
saolini1933076 <133365462+saolini1933076@users.noreply.github.com>
2023-12-06 11:10:54 +0100

    Update fake_os.h

--- fake_os.h
+++ fake_os.h
@@ -1,27 +1,35 @@
 #include "fake_process.h"
 #include "linked_list.h"
+#define MAX 99999
 #pragma once
 
-struct FakeOS;  // Dichiarazione forward di FakeOS
 
 typedef struct {
   ListItem list;
   int pid;
   ListHead events;
+  int arrival_time;
+  float burst_prediction;
+  int last_burst;
+  float last_prediction;
 } FakePCB;
 
-typedef struct FakeOS {
-  FakePCB* running;
+struct FakeOS;
+typedef void (*ScheduleFn)(struct FakeOS* os, void* args);
+
+typedef struct FakeOS{
+  ListHead running;
   ListHead ready;
   ListHead waiting;
   int timer;
-  void (*schedule_fn)(struct FakeOS* os, void* args);
+  ScheduleFn schedule_fn;
   void* schedule_args;
+
+  float decay_coefficient;
+  int num_cpus;
   ListHead processes;
-  int num_cpus;  
-  struct FakeOS** cpus;
 } FakeOS;
 
-void FakeOS_init(FakeOS* os, int num_cpus);  // Rimuovi 'struct' dai parametri
+void FakeOS_init(FakeOS* os);
 void FakeOS_simStep(FakeOS* os);
 void FakeOS_destroy(FakeOS* os);
